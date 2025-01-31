2dc7ce73cabe6688a66b9172e1cade7e0e6ff7dc
IgorAleksejevTTHK <igoralekseje@gmail.com>
2025-01-31 16:46:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta oma nimi ja vajuta enter");
+            string name = Console.ReadLine();
+
+            if (name !="")
+            {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Tere, " + name);
+            }
+            else
+            {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("error");
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            }
+            }
+        }
+    }
